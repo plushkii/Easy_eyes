aeec6da098d58de55219f74dae563f9760f45ec8
plushkii <122995106+plushkii@users.noreply.github.com>
2023-11-13 21:31:36 +0400

    Add files via upload

---
+++ advices.py
@@ -0,0 +1,26 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QWidget
+
+
+class AdviceForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('advises.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.read()
+
+    def read(self):
+        file = open('advice.txt', encoding='utf8')
+        txt = file.read()
+        self.textEdit.setPlainText(txt)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = AdviceForm()
+    ex.show()
+    sys.exit(app.exec())
